5d7975f9c43163f82a519360268c340c7c177e9d
steven_hp_1 <s@s.com>
2025-12-21 14:59:28 -0800

    adds new run and update functions that can be used by the user

--- src/l01/__init__.py
+++ src/l01/__init__.py
@@ -4,3 +4,21 @@ from .Image_Rect import Image_Rect
 from .global_vars import *
 IR = Image_Rect
 
+import inspect
+
+def run(width, height):
+    init(width, height)
+    
+    # Get the caller's global namespace
+    frame = inspect.currentframe().f_back
+    user_globals = frame.f_globals
+    
+    # Look for their update() function
+    user_update = user_globals.get('update')
+    
+    while True:
+        process_events()
+        if user_update:
+            user_update()
+        update_screen()
+
