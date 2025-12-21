55dfdc931b9f591b340c7a3cb6a8f7bd2bd4c696
steven_hp_1 <s@s.com>
2025-12-21 15:27:26 -0800

    major change: user can use a decorator for the update function

--- src/l01/__init__.py
+++ src/l01/__init__.py
@@ -4,21 +4,35 @@ from .Image_Rect import Image_Rect
 from .global_vars import *
 IR = Image_Rect
 
-import inspect
+# Store registered functions
+_user_update = None
+_window_size = (400, 400)
 
-def run(width, height):
-    init(width, height)
-    
-    # Get the caller's global namespace
-    frame = inspect.currentframe().f_back
-    user_globals = frame.f_globals
-    
-    # Look for their update() function
-    user_update = user_globals.get('update')
-    
-    while True:
-        process_events()
-        if user_update:
-            user_update()
-        update_screen()
+def game(func):
+	"""Decorator to register the game's update function and start the game loop."""
+	global _user_update
+	_user_update = func
+	run()
+	return func
+
+def update(func):
+	"""Decorator to register the update function without auto-starting."""
+	global _user_update
+	_user_update = func
+	return func
+
+def set_window(x: int, y: int):
+	"""Set the window size before running."""
+	global _window_size
+	_window_size = (x, y)
+
+def run():
+	"""Start the game loop."""
+	init(_window_size[0], _window_size[1])
+
+	while True:
+		process_events()
+		if _user_update:
+			_user_update()
+		update_screen()
 
